0191e040917dfb0a6d1640c44b9c13ca667779c3
Demonyker <demonyker14@gmail.com>
2019-05-21 14:00:59 +0300

    better

--- js.js
+++ js.js
@@ -1,14 +1,19 @@
-var i = 0
-var arr = [100, 100, 200, 300, 100, -400]
-var sum = 0
-var maxNumber = 0
+var arr = [-1, -1, 20, 9]
 function maxsum(arr) {
-        for (i; i < arr.length; i++) {
-            sum = sum + arr[i]
-            if (sum > maxNumber) {
-             maxNumber = sum 
-            }
+    let sum = 0
+    let maxNumber = 0 
+    for ( let i = 0; i < arr.length; i++) {
+        sum = arr[i] 
+        if (sum > maxNumber) {
+        maxNumber = sum 
         }
+        for (let j = i+1; j<arr.length; j++) {
+            sum= sum+arr[j]
+            if (sum > maxNumber) {
+                maxNumber = sum 
+                }
+        }
+    }
+console.log(maxNumber)
 }
 maxsum(arr)
-console.log(maxNumber)
--- js1.js
+++ js1.js
@@ -5,26 +5,14 @@ var pos2 = prompt('введите вторую позицию')
 var slon = (arg1, arg2) => {
    let arg1n = arg1.split('')
    let arg2n = arg2.split('')
-    if ( arg1n[0] === arg2n[0] || arg1n[1] === arg2n[1]){
-    return result = false;
-}
-   for (let i = 0; i < alf.length; i++) {
-       if ( arg1n[0] === alf[i] && (arg2n[0] === alf[i+1] || arg2n[0] === alf[i-1]) && (+arg2n[1] === (+arg1n[1] + 1)) || +arg2n[1] === (+arg1n[1] - 1) ){
-       return result = true;
-       } else if ( arg1n[0] === alf[i] && (arg2n[0] === alf[i+2] || arg2n[0] === alf[i-2]) && (+arg2n[1] === (+arg1n[1] + 2)) || +arg2n[1] === (+arg1n[1] - 2) ) {
-       return result = true;
-       } else if ( arg1n[0] === alf[i] && (arg2n[0] === alf[i+3] || arg2n[0] === alf[i-3]) && (+arg2n[1] === (+arg1n[1] + 3)) || +arg2n[1] === (+arg1n[1] - 3) ) {
-       return result = true;
-       } else if ( arg1n[0] === alf[i] && (arg2n[0] === alf[i+4] || arg2n[0] === alf[i-4]) && (+arg2n[1] === (+arg1n[1] + 4)) || +arg2n[1] === (+arg1n[1] - 4) ) {
-       return result = true;
-    } else if ( arg1n[0] === alf[i] && (arg2n[0] === alf[i+5] || arg2n[0] === alf[i-5]) && (+arg2n[1] === (+arg1n[1] + 5)) || +arg2n[1] === (+arg1n[1] - 5) ) {
-       return result = true;
-    } else if ( arg1n[0] === alf[i] && (arg2n[0] === alf[i+6] || arg2n[0] === alf[i-6]) && (+arg2n[1] === (+arg1n[1] + 6)) || +arg2n[1] === (+arg1n[1] - 6) ) {
-       return result = true;
-    } else if ( arg1n[0] === alf[i] && (arg2n[0] === alf[i+7] || arg2n[0] === alf[i-2]) && (+arg2n[1] === (+arg1n[1] + 7)) || +arg2n[1] === (+arg1n[1] - 7) ) {
-       return result = true;
-    } 
-}
+   let arg1nP1 = arg1n[0].charCodeAt(0)
+   let arg1nP2 = arg2n[0].charCodeAt(0)
+   if ( (arg1n[1] - arg1nP1) === (arg2n[1] - arg1nP2) ) {
+     result = true
+   } else { 
+     result = false
+   }
+   console.log(result)
 }
 slon(pos1, pos2)
-console.log(result)
+
