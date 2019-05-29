bf927a18b9063667edc8541d300fc2cbf12e1223
Demonyker <demonyker14@gmail.com>
2019-05-29 14:47:46 +0300

    first commmit oop

--- ooptask.js
+++ ooptask.js
@@ -1,95 +1,163 @@
 class Director {
-    constructor(name, age) {
-    this.name = name,
-    this.age = age,
-    this.projects = 0,
-    this.staff = 0
-    }
-    getProjects(projects){
-        if (projects.length > 4){
-            console.log('Слишком много проектов, не могу принять')
-        }else{
-        this.projects = projects  
-        }
-    console.log(this.projects)
-    }
-    getStaff (staffPep) {
-        this.staff = staffPep
-        console.log(`Количество нанятых сотрудников - ${this.staff}`)
-    }
-    getRabotnik (Rabotnik, name, otdel) {
-        return new Rabotnik(name, otdel)
-    }
-}
-class Rabotnik {
-    constructor(name, otdel){
-    this.name = name,
-    this.otdel = otdel,
-    this.realizePr = 0,
-    this.status
-    }
-    getProject () {
-        this.realizePr++
-        this.status = busy
-    }
-}
-function workCompany(days) { //ноль проектов, ноль программистов
-    var realizePr = 0
-    var newStaff = 0
-    var rmStaff = 0
-    var rabotniki = []
-    var denis = new Director('Denis', 35)
-    var projects = []
-for (var j = 1; j<(days+1); j++){
-    if (denis.projects > 0){
-        denis.projects.forEach( function(item){
-        let t = item.split(', ');
-        var theme = t[0]
-        if( theme == web){
-            var name = prompt('Введите имя сотрудника')
-            var otdel = web
-            name = denis.getRabotnik(Rabotnik, name, otdel)
-            rabotniki[i] = name
-            } else {
-            var name = prompt('Введите имя сотрудника')
-            var otdel = mobile
-            name = denis.getRabotnik(Rabotnik, name, otdel)
-            rabotniki[i] = name    
-            }    
-        })   
-    }
-    let numofPr = prompt('Сколько новых проектов?')
-    if (numofPr > 4) { 
-    console.log('Слишком много проектов')
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.projects = 0;
+    this.progers = [];
+  }
+
+  getProjects(projects) {
+    if (projects.length > 4) {
+      console.log('Слишком много проектов, не могу принять');
     } else {
-    for (let i = 0; i<numofPr; i++) {
-    projects.push(prompt('Введите проекты для'+'  ' + j +'  '+'дня сложность укажите через запятую'))
-    denis.getProjects(projects)
+      this.projects = projects;
     }
-    }
-    if (denis.projects > 0 && rabotniki.length > 0) {  
-    for( let i = 0 ; i<denis.projects.length; i++){
-        denis.projects.forEach( function(item){
-        let t = item.split(', ');
-        var theme = t[0]
-        var slojnost = t[1]
-        
-        })      
-    }
-    }
+  }
+
+  getProgers(Proger, id, spec) {
+    this.progers.push(new Proger(id, spec));
+  }
+}
+class PartOfCompany {
+  constructor(progers, projects) {
+    this.progers = progers;
+    this.projects = projects;
+  }
+}
+class WebDev extends PartOfCompany {
+  constructor(progers, projects) {
+    super(progers, projects);
+    this.spec = 'web';
+  }
+
+  takeProject(i) {
+    this.progers[i].projects += 1;
+    this.progers[i].status = 'busy';
+    this.progers[i].exp += 1;
+    this.project[i] = `На этом проекте работает программист ${this.progers[i].id}`;
+  }
+}
+class MobDev extends PartOfCompany {
+  constructor(progers, projects) {
+    super(progers, projects);
+    this.spec = 'mobile';
+  }
+
+  takeProjectOnPr(i) {
+    this.progers[i].projects += 1;
+    this.progers[i].status = 'busy';
+    this.progers[i].exp += 1;
+  }
+
+  takeProjectOnPart(i) {
+    this.project[i] = `На этом проекте работает программист ${this.progers[i].id}`;
+  }
+}
+class Testers extends PartOfCompany {
+  constructor() {
+    super();
+    this.spec = 'testers';
+  }
+}
+class Proger {
+  constructor(id, spec) {
+    this.id = id;
+    this.spec = spec;
+    this.exp = 0;
+    this.curPr = 0;
+    this.status = 'free';
+  }
 }
 
 
-
-
-
-    /*for (let i = 0; i< 2; i++) {
-    var name = prompt('Введите имя сотрудника')
-    var otdel = prompt('Введите отдел сотрудника')
-    name = denis.getRabotnik(Rabotnik, name, otdel)
-    name.getTest()
-    rabotniki[i] = name
-    }*/
+/* let arrT = [];
+arrT.push(new Proger(1, 'mob'));
+arrT.push(new Proger(2, 'web'));
+arrT.push(new Proger(3, 'mob'));
+arrT.push(new Proger(4, 'web'));
+arrT.push(new Proger(5, 'web'));
+let diff = 3;
+let test = 0;
+  arrT.some((key, k) => {
+    if (key.spec === 'web') {
+    console.log(key.id);
+    test += 1;
+    }
+    if (test === diff) {
+      return test;
+    }
+});
+*/
+function workCompany(days, numofPr, massOfPr) { // ноль проектов, ноль программистов
+  let realizePr = 0;
+  let newStaff = 0;
+  let rmStaff = 0;
+  let rabotniki = [];
+  let mobRab;
+  const denis = new Director('Denis', 35);
+  const web = new WebDev();
+  const mob = new MobDev();
+  const testers = new Testers();
+  let projects = [];
+  for (let j = 0; j < days; j += 1) {
+    let diff = 0; // очень нужно для работы моб. отдела
+    let naim = 0; // количество нанятых работников
+    if (denis.projects.length > 0) {
+      denis.projects.forEach((item) => {
+        const Pr = item.split(', ');
+        const theme = Pr[0];
+        const difficulty = Pr[1];
+        if (theme === 'web') {
+          denis.getProgers(Proger, naim, theme);
+          naim += 1;
+        } else if (theme === 'mob') {
+          for (let naimR = 0; naimR < difficulty; naimR += 1) {
+            denis.getProgers(Proger, naim, theme);
+            naim += 1;
+          }
+        }
+      });
+    }
+    if (numofPr > 4) {
+      console.log('Слишком много проектов');
+    } else {
+      for (let i = 0; i < numofPr; i += 1) {
+        projects.push(massOfPr[i]);
+        denis.getProjects(projects);
+      }
+    }
+    if (denis.projects.length > 0) {
+      denis.projects.forEach((item, i) => {
+        const Pr = item.split(', ');
+        const theme = Pr[0];
+        const difficulty = Pr[1];
+        if (theme === 'web' && (web.progers.length > 0)) {
+          web.progers.some((key, k) => {
+            if (key.status === 'free') {
+              web.takeProject(k);
+              denis.projects.splice(i, 1);
+            }
+            return k;
+          });
+        } else if (theme === 'mob' && (mob.progers.length > 0) && (mob.progers.filter(rabotnik => rabotnik.status === 'free')).length >= +difficulty) {
+          mob.progers.some((key, k) => {
+            if (key.status === 'free') {
+              mob.takeProjectOnPr(k);
+              mobRab.push(mob.progers[k].id);
+              diff += 1;
+            }
+            if (diff === +difficulty) {
+              mobRab.forEach((idRab, l) => {
+              
+              })
+              mob.takeProjectOnPart();
+              denis.projects.splice(i, 1);
+              return diff;
+            }
+          });
+        }
+      });
+    }
+  }
 }
-workCompany(2)
-
+workCompany(2);
